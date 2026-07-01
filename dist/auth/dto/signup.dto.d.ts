c3bff4cdc49d0c73faa257c8c4ac35f93d6fdce4
Sujith Rajan <140044515+Sujith-Rajan@users.noreply.github.com>
2026-07-01 15:05:53 +0530

    Delete dist directory

--- dist/auth/dto/signup.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class SignupDto {
-    name: string;
-    email: string;
-    phone?: string;
-    password: string;
-}
--- dist/common/decorators/roles.decorator.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const ROLES_KEY = "role";
-export declare const Roles: (...role: string[]) => import("@nestjs/common").CustomDecorator<string>;
